6664d19b1abd60dad33e5c7216833be310b73a4f
Nicolas Unterberger <nicounterberger@yahoo.at>
2021-03-14 18:09:38 +0100

    Server und Buttons implementiert

--- app/src/main/java/com/example/se2_einzelabgabe_unterberger/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelabgabe_unterberger/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.se2_einzelabgabe_unterberger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+    public void messageSend(View view) {
+        EditText nr_bearbeiten= findViewById(R.id.nr_bearbeiten);
+        String text = nr_bearbeiten.getText().toString();
+        TextView tv = findViewById(R.id.text_output);
+
+        try {
+            thread_tcp thread = new thread_tcp(text);
+            thread.start();
+
+            thread.join();
+
+            tv.setText(thread.getServer_output());
+
+        } catch (Exception e) {
+            tv.setText(e.toString());
+        }
+}}
---
+++ app/src/main/java/com/example/se2_einzelabgabe_unterberger/thread_tcp.java
@@ -0,0 +1,73 @@
+package com.example.se2_einzelabgabe_unterberger;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class thread_tcp extends Thread {
+    private String nachricht;
+    private String server_output;
+    static Socket clientSocket = null;
+
+
+    private static final int SERVERPORT = 53212;
+    private static final String SERVER = "se2-isys.aau.at";
+
+
+    thread_tcp(String nachricht) {
+        this.nachricht = nachricht;
+    }
+
+    public String getServer_output() {
+        return this.server_output;
+    }
+
+    @Override
+
+
+    public void run() {
+        try {
+            clientSocket = new Socket(SERVER, SERVERPORT);
+            try {
+                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                outToServer.writeBytes(nachricht + "\n");
+                server_output = inFromServer.readLine();
+            } catch (Exception e) {
+                Log.e("fail",e.toString());
+                throw e;
+            }
+
+
+            finally {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            }
+        } catch (UnknownHostException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
